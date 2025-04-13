9d428791deb9eb617da2560d1b433e89067b686b
LasNoches888 <igor.tepliakow@yandex.ru>
2025-04-13 15:44:52 +0500

    Почти дописал два стека

--- Stacks_Iterators.h
+++ Stacks_Iterators.h
@@ -2,6 +2,7 @@
 #define STACKS_ITERATORS_H_INCLUDED
 using namespace std;
 #include <iostream>
+#include <vector>
 enum class RaceType
 {
     Bad_races = 1,
@@ -66,5 +67,39 @@ public:
     virtual T GetCurrent() = 0;
 };
 
+
+const size_t MaxSize = 100;
+
+template<typename T>
+class Massive_Stack
+{
+private:
+    T* Things = new T[MaxSize];  // Создание массива, который будет содержать данные типа T
+    size_t Top; //Индекс элемента стека, следующего за верхним
+
+public:
+    Massive_Stack() : Top(0) {}
+    T Pop() {return Things[--Top];}
+    bool IsEmpty() const  {return (Top == 0);}
+
+
+    void Push(T NewThing) {Things[Top++] = NewThing;} //Добавление элемента
+    T GetElementByIndex(size_t index) { return Things[index];} //Получение элемента по индексу
+    ~Massive_Stack() { delete [] Things;} //Декструктор, чтобы чистить память
+};
+
+
+template<typename T>
+class Vector_stack
+{
+private:
+    vector<T> Items[MaxSize];
+    size_t top;
+public:
+    Vector_stack() : top(0) {}
+    void Add(T NewItem) {Items.push_back(NewItem);}
+    T GetElement(size_t Index) {return Items[Index];}
+    void Ret() {return Items.pop_back();}
+};
 
 #endif // STACKS_ITERATORS_H_INCLUDED
