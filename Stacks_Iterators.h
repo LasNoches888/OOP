b16adff85106668218d473b98be5d95088bf8319
LasNoches888 <igor.tepliakow@yandex.ru>
2025-04-18 18:10:07 +0500

    Наконец-то рабочая версия программы

--- Stacks_Iterators.h
+++ Stacks_Iterators.h
@@ -17,6 +17,9 @@ public:
 };
 
 
+template <typename T> class Massive_Stack_Iterator; //Forwarding declaration - заранее обьявляем итератор для массива
+template <typename T> class Vector_Iterator; //То же самое, что и выше, только для вектора
+
 const size_t MaxSize = 100;
 
 template<typename T>
@@ -35,6 +38,11 @@ public:
     void Push(T NewThing) {Things[Top++] = NewThing;} //Добавление элемента
     T GetElementByIndex(size_t index) { return Things[index];} //Получение элемента по индексу
     ~Massive_Stack() { delete [] Things;} //Декструктор, чтобы чистить память
+    Iterator<T>* GetIterator()
+    {
+        return new Massive_Stack_Iterator<T>(this);
+    }
+
 };
 
 
@@ -48,7 +56,13 @@ public:
     Vector_stack() : top(0) {}
     void Add(T NewItem) {Items.push_back(NewItem);}
     T GetElement(size_t Index) {return Items[Index];}
-    void Ret() {return Items.pop_back();}
+    size_t Size() const { return Items.size(); }
+
+    Iterator<T>* GetIterator()
+    {
+        return new Vector_stack<T>(Items, top);
+    }
+
 };
 
 
@@ -61,7 +75,7 @@ class Massive_Stack_Iterator : public Iterator<T>
 
 public:
     Massive_Stack_Iterator(Massive_Stack<T>* stak)
-    : stak(stak) {}
+    : stak(stak), Pos(0), mass_size(stak->SizeStack()) {}
 
 
     void First() override { Pos = 0; }
@@ -74,13 +88,51 @@ public:
 template<typename T>
 class Vector_Iterator : public Iterator<T>
 {
-    T* Vector_stack;
+    Vector_stack<T>* data;
     size_t pos;
     size_t vector_size;
 public:
+    Vector_Iterator(Vector_stack<T>* data) : data(data), pos(0), vector_size(data->Size()) {}
     void First() override { pos = 0; }
     void Next() override { pos++; }
     bool IsDone() const override { return (pos >= vector_size); }
-    T GetCurrent() const override { return Vector_stack[pos]; }
+    T GetCurrent() const override { return data->GetElement(pos); }
 };
+
+template<typename T>
+class IteratorDecorator : public Iterator<T>
+{
+protected:
+    Iterator<T> *It;
+public:
+    IteratorDecorator(Iterator<T> *it) : It(it) {}
+    virtual ~IteratorDecorator() { delete It; }
+    virtual void First() { It->First(); }
+    virtual void Next() { It->Next(); }
+    virtual bool IsDone() const { return It->IsDone(); }
+    virtual T GetCurrent() const { return It->GetCurrent(); }
+};
+
+template<typename ContainerType, typename ItemType>
+class ConstIteratorAdapter : public Iterator<ItemType>
+{
+protected:
+    ContainerType *Container;
+    typename ContainerType::const_iterator It;
+
+public:
+    ConstIteratorAdapter(ContainerType *container)
+    : Container(container)
+    {
+        It = Container->begin();
+    }
+
+    virtual ~ConstIteratorAdapter() {}
+    virtual void First() { It = Container->begin(); }
+    virtual void Next() { It++; }
+    virtual bool IsDone() const { return (It == Container->end()); }
+    virtual ItemType GetCurrent() const { return *It; }
+};
+
+
 #endif // STACKS_ITERATORS_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Stacks_Iterators.h"
 #include <vector>
+#include <list>
 using namespace std;
 
 
@@ -21,11 +22,17 @@ public:
     string name;
     int year;
     string place;
-    Middle_Earth(string n, int y, string p) : name(n), year(y), place(p) {}
+    Middle_Earth(string n, int y, string p, RaceType s) : name(n), year(y), place(p), side(s) {}
 
     virtual void Print() const = 0;
     ~Middle_Earth() {}
     virtual RaceType GetSide() const {return side;}
+
+    bool IsGood() const
+    {
+        return side == RaceType::Elves || side == RaceType::Humans;
+    }
+
 };
 
 class Elves : public Middle_Earth
@@ -36,7 +43,7 @@ public:
     bool in_films;
 
     Elves(string n = "Elves", int y = 1000, string p = "West", string k = "Rivendel", int v = 10000, bool in = true)
-     : Middle_Earth(n, y, p), kind_of_elves(k), values(v), in_films(in) {}
+     : Middle_Earth(n, y, p, RaceType::Elves), kind_of_elves(k), values(v), in_films(in) {}
      void Print() const override
      {
          cout<<"One of races: "<<name<<endl;
@@ -52,7 +59,7 @@ public:
     bool win_or_not;
 
     Bad_races(string n = "Orcs", int y = 1500, string p = "Mordor", string koe = "Mordor's orcs", int h = 50000, bool w = false )
-     : Middle_Earth(n, y, p), kind_of_evil(koe), howmany(h), win_or_not(w) {}
+     : Middle_Earth(n, y, p, RaceType::Bad_races), kind_of_evil(koe), howmany(h), win_or_not(w) {}
     void Print() const override
      {
          cout<<"One of races: "<<name<<endl;
@@ -69,7 +76,7 @@ public:
 
 
     Human(string n = "Humans", int y = 2000, string p = "Gondor", string p_side = "With good mans", int val_arnor = 1 )
-     : Middle_Earth(n, y, p), Parents_side(p_side), value_of_Arnors_sons(val_arnor) {}
+     : Middle_Earth(n, y, p, RaceType::Humans), Parents_side(p_side), value_of_Arnors_sons(val_arnor) {}
         void Print() const override
      {
          cout<<"One of races: "<<name<<endl;
@@ -106,6 +113,47 @@ void CommonPrint(Iterator<Middle_Earth*> *it)
     }
 }
 
+
+void RaceGood(Iterator<Middle_Earth*> *it)
+{
+    for(it->First(); !it->IsDone(); it->Next())
+    {
+        Middle_Earth *current_race = it->GetCurrent();
+        if(!current_race->IsGood()) continue;
+
+        current_race->Print();
+    }
+}
+
+
+class RaceGoodDecorator : public IteratorDecorator<Middle_Earth*>
+{
+private:
+    bool TargetGood;
+
+public:
+    RaceGoodDecorator(Iterator<Middle_Earth*> *it, bool isGood)
+        : IteratorDecorator<Middle_Earth*>(it), TargetGood(isGood) {}
+
+    void First() override
+    {
+        It->First();
+        while (!It->IsDone() && It->GetCurrent()->IsGood() != TargetGood)
+        {
+            It->Next();
+        }
+    }
+
+    void Next() override
+    {
+        do
+        {
+            It->Next();
+        } while (!It->IsDone() && It->GetCurrent()->IsGood() != TargetGood);
+    }
+};
+
+
 int main()
 {
     cout<<"Enter the value of orcs: ";
@@ -132,19 +180,59 @@ int main()
         Middle_Earth *new_bad = CreateRace(type_1_ob);
         Bad_stack.Push(new_bad);
     }
-    Iterator<Middle_Earth*> *it = new Massive_Stack_Iterator<Middle_Earth*>(&Bad_stack);
-    CommonPrint(it);
-    delete it;
+
 
 
     Vector_stack<Middle_Earth*> Good_stack;
-    for(size_t i = 0; i!=orc_value; i++)
+    for(size_t i = 0; i!=good_value; i++)
     {
         Middle_Earth *new_good = CreateRace(type_2_ob);
         Good_stack.Add(new_good);
     }
 
+    Iterator<Middle_Earth*> *it = new Massive_Stack_Iterator<Middle_Earth*>(&Bad_stack);
+    CommonPrint(it);
+    delete it;
+    Iterator<Middle_Earth*> *it2 = new Vector_Iterator<Middle_Earth*>(&Good_stack);
+    CommonPrint(it2);
+    delete it2;
+
+
+    Massive_Stack<Middle_Earth*> All_Races;
+    for(size_t i=0; i<10; i++)
+    {
+        int rac_num1 = rand()%2+1; // Число от 1 до 3 (случайный фрукт)
+        RaceType Race_type = static_cast<RaceType>(rac_num1);
+        Middle_Earth *newrace1 = CreateRace(Race_type);
+        All_Races.Push(newrace1);
+    }
+
+
+    cout << endl << "Show all good races with iterator:" << endl;
+    Iterator<Middle_Earth*> *goodIt = new RaceGoodDecorator(All_Races.GetIterator(), true);
+    RaceGood(goodIt);
+    delete goodIt;
+
+
+    list<Middle_Earth*> fruitVector; // а еще можно vector, forward_list, ...
+    for(size_t i=0; i<10; i++)
+    {
+        int rac_num1 = rand()%2+1; // Число от 1 до 3 (случайный фрукт)
+        RaceType Race_type = static_cast<RaceType>(rac_num1);
+        Middle_Earth *newrace2 = CreateRace(Race_type);
+        fruitVector.push_back(newrace2); // Добавить новый фрукт в конец списка
+    }
+
+
+    cout << "Eating all good orange using adapted iterator (another container):" << endl;
+    Iterator<Middle_Earth*> *adaptedIt = new ConstIteratorAdapter<std::list<Middle_Earth*>, Middle_Earth*>(&fruitVector);
+    Iterator<Middle_Earth*> *adaptedOrangeIt = new RaceGoodDecorator(adaptedIt, true);
+    RaceGood(adaptedOrangeIt);
+    delete adaptedOrangeIt;
+
 }
+
+
 
 
 
